c4f7bcd76c2ed18d3f5bce8c7313891a25d47b5e
hnist <369365576@qq.com>
2021-06-13 14:15:05 +0800

    6.13zdk 修复注册验证码bug

--- src/components/user/PwdChange.vue
+++ src/components/user/PwdChange.vue
@@ -69,6 +69,13 @@ export default {
          callback();
       }
      };
+    const checkCode = (rule, value, cb) => {
+      const regCode = this.verification
+      if (regCode === value) {
+        return cb()
+      }
+      cb(new Error('请输入正确的验证码'))
+    };
     const checkEmail = (rule, value, cb) => {
       const regEmail = /^([a-zA-Z0-9_-])+@([a-zA-Z0-9_-])+(\.[a-zA-Z0-9_-])+/
       if (regEmail.test(value)) {
@@ -101,6 +108,10 @@ export default {
         ],
         rule:[
           { required: true, message: '请填写角色框！', trigger: 'blur'}
+        ],
+        code:[
+          { required: true, message: '请填写验证码', trigger: 'blur'},
+          { validator: checkCode, trigger: 'blur' }
         ]
       },
       radio:1,
